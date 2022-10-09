3b9ce63e517e6df19e277d8b917f8d37297086f9
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-10-09 22:38:05 +0200

    initialize the class Post

---
+++ lib/post.rb
@@ -0,0 +1,5 @@
+class Post
+  def initialize
+    @post_read_completed = false
+  end
+end
---
+++ spec/post_spec.rb
@@ -0,0 +1,11 @@
+require "post"
+
+RSpec.describe Post do
+  it "returns true when post is read" do
+    expect(subject.mark_post_as_read).to be true
+  end
+
+  it "returns true for a read post" do
+    expect(subject.post_read?).to be true
+  end
+end
